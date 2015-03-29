3153ac206551898adea957ad1c93f0a7a6ad46c0
Stefano Pogliani <stefano@spogliani.net>
2015-03-29 18:01:39 +0100

    Started migrating state classes to library.

---
+++ grunt-modules/state.js
@@ -0,0 +1,9 @@
+module.exports = function(grunt_module) {
+
+  require("../utils/cpp-module")(grunt_module, {
+    code: "snow-fox-state/",
+    name: "state",
+    path: "netbeans-projects/snow-fox-state/"
+  });
+
+};
---
+++ grunt-modules/utils.js
@@ -0,0 +1,9 @@
+module.exports = function(grunt_module) {
+
+  require("../utils/cpp-module")(grunt_module, {
+    code: "snow-fox-utils/",
+    name: "utils",
+    path: "netbeans-projects/snow-fox-utils/"
+  });
+
+};
